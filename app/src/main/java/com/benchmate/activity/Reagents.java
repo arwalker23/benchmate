c774f2302cb9a5ec92e9d6c78f78c2efa71fe4c7
Mihai Chelaru <34112608+mihaichelaru@users.noreply.github.com>
2018-04-15 15:43:45 -0400

    added naming of wells for CSV
    Signed-off-by: MihaiChelaru <mihai72@gmail.com>

--- app/src/main/java/com/benchmate/activity/Reagents.java
+++ app/src/main/java/com/benchmate/activity/Reagents.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.benchmate.R;
 import com.benchmate.domain.Experiment;
@@ -22,10 +22,9 @@ public class Reagents extends Activity implements CompoundButton.OnCheckedChange
 
     int flag = 0;
     Button buttonBack, buttonSave;
-    //    ArrayList<String> selectedReagents = new ArrayList<>();
-//    ArrayList<String> retrievedSelectedReagents;
     ArrayList<Boolean> checkedReagents;
     ArrayList<CheckBox> checkboxArray = new ArrayList<>();
+    EditText wellLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +35,13 @@ public class Reagents extends Activity implements CompoundButton.OnCheckedChange
         // Retrieve experiment object from intent
         Intent intent = getIntent();
         final Experiment experiment = (Experiment) intent.getSerializableExtra("experiment");
+
         // Retrieve name of well so Reagents knows which Well object to modify
         final String wellName = intent.getStringExtra("wellName");
+
         // Debug toast
-        Toast.makeText(this, "Now viewing well " + experiment.getWells().get(wellName).getName(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "Now viewing well " + experiment.getWells().get(wellName).getName(), Toast.LENGTH_SHORT).show();
+
         // Retrieve checked boxes from experiment class TreeMap "wells"
         checkedReagents = new ArrayList<>(experiment.getWells().get(wellName).getSelectedReagents());
 
@@ -48,6 +50,9 @@ public class Reagents extends Activity implements CompoundButton.OnCheckedChange
             checkedReagents = (ArrayList<Boolean>) savedInstanceState.getSerializable("checkedReagents");
             flag = savedInstanceState.getInt("savedflag");
         }
+
+        wellLabel = findViewById(R.id.wellName);
+        wellLabel.setText(experiment.getWells().get(wellName).getName());
 
         buttonBack = findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +72,7 @@ public class Reagents extends Activity implements CompoundButton.OnCheckedChange
                 Intent intent = new Intent(Reagents.this, Plate.class);
                 // Handling for selectedFields array, can update the Experiment object TreeMap "wells" accordingly
                 experiment.getWells().get(wellName).setSelectedReagents(checkedReagents);
+                experiment.getWells().get(wellName).setName(wellLabel.getText().toString());
                 intent.putExtra("experiment", experiment);
                 startActivity(intent);
                 finish();
